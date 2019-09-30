72eb6dc8a8d3f5174b3b6c4612292cdc7fede89f
Tuoris <tuoris.dev@gmail.com>
2019-09-30 21:31:33 +0300

    Implement image uploading for account

--- django_accounts/api/serializers.py
+++ django_accounts/api/serializers.py
@@ -1,7 +1,14 @@
+import base64
+import re
+from io import BytesIO
+
+from django.core.files.base import ContentFile
+from PIL import Image
 from rest_framework import serializers
 
 from api.models import Account
 
+MAX_IMAGE_SIZE = 256  # in pixels
 
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,3 +20,37 @@ class AccountSerializer(serializers.ModelSerializer):
             'last_name',
             'creation_date',
         ]
+
+
+class JSONCreateAccountSerializer(serializers.ModelSerializer):
+    avatar = serializers.CharField()
+
+    class Meta:
+        model = Account
+        fields = [
+            'id',
+            'avatar',
+            'first_name',
+            'last_name',
+            'creation_date',
+        ]
+
+    def create(self, validated_data):
+        image_data = validated_data.pop('avatar')
+        if image_data:
+            image_data = re.sub('^data:image/.+;base64,', '', image_data)
+            image = Image.open(BytesIO(base64.b64decode(image_data)))
+            im_width, im_height = image.size
+            if im_width > im_height:
+                scale_coeficient = MAX_IMAGE_SIZE / im_width
+            else:
+                scale_coeficient = MAX_IMAGE_SIZE / im_height
+            image = image.resize((int(im_width * scale_coeficient), int(im_height * scale_coeficient)))
+            image = image.convert('RGB')
+
+            buffer = BytesIO()
+            image.save(buffer, 'JPEG')
+            image_file = ContentFile(buffer.getvalue(), 'upload.jpg')
+
+            validated_data['avatar'] = image_file
+        return Account.objects.create(**validated_data)
--- django_accounts/api/views.py
+++ django_accounts/api/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
 from api.models import Account
-from api.serializers import AccountSerializer
+from api.serializers import AccountSerializer, JSONCreateAccountSerializer
 
 
 class AccountView(viewsets.ModelViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
+
+    def get_serializer_class(self):
+        if self.action == 'create' and self.request.content_type == 'application/json':
+            return JSONCreateAccountSerializer
+        return AccountSerializer
