b9d70341f5b74a756c275cec7f724b8b08caf9d7
Tuoris <tuoris.dev@gmail.com>
2019-09-30 17:04:31 +0300

    Add functionality to display accounts and delete them

--- django_accounts/frontend/src/App.vue
+++ django_accounts/frontend/src/App.vue
@@ -1,27 +1,70 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-2" id="app">
+    <div class="row">
+      <Account
+        v-for="account in accounts"
+        v-bind:account="account"
+        v-bind:key="account.id"
+        v-on:edit="editAccount"
+        v-on:delete="deleteAccount"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Account from "./components/Account.vue";
 
 export default {
-  name: 'app',
+  name: "app",
+  data: function() {
+    return {
+      accounts: []
+    };
+  },
+
   components: {
-    HelloWorld
+    Account,
+    AddAccount,
+  },
+
+  methods: {
+    editAccount: function(account) {
+      console.log(account);
+    },
+
+    deleteAccount: function(account) {
+      this.accounts = this.accounts.filter(
+        existing_account => existing_account.id !== account.id
+      );
+      fetch("api/accounts/" + account.id, { method: "DELETE" }).then(
+        response => {
+          if (!response.ok) {
+            this.accounts.push(account);
+          }
+        }
+      );
+    },
+
+    fetchAccounts: function() {
+      fetch("api/accounts")
+        .then(response => response.json())
+        .then(json_data => (this.accounts = json_data));
+    }
+  },
+
+  mounted() {
+    this.fetchAccounts();
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ django_accounts/frontend/src/components/Account.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card col-md-3 m-2 p-0">
+    <div class="avatar-image-container">
+      <img v-if="account.avatar" v-bind:src="account.avatar" class="avatar-image card-img-top" />
+      <img v-else src="/static/no-avatar.png" class="avatar-image card-img-top" />
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">{{ account.first_name }} {{ account.last_name }}</h5>
+      <p class="card-text">Created {{ account.creation_date }}</p>
+      <div class="btn-group mr-1 ml-1">
+        <button v-on:click="deleteAccount" class="btn btn-danger">Delete</button>
+      </div>
+      <div class="btn-group mr-1 ml-1">
+        <button v-on:click="editAccount" class="btn btn-primary">Edit</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Account",
+  props: {
+    account: Object
+  },
+  methods: {
+    editAccount: function(event) {
+      this.$emit('edit', this.account);
+    },
+    deleteAccount: function(event) {
+      this.$emit('delete', this.account);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.avatar-image-container {
+  overflow: hidden;
+  padding-bottom: 80%;
+  position: relative;
+}
+
+.avatar-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
