d0c54a25bcf999466b574413adbe7e36166a8e4a
Tuoris <tuoris.dev@gmail.com>
2019-09-30 14:37:15 +0300

    Initial frontend setup

--- django_accounts/django_accounts/urls.py
+++ django_accounts/django_accounts/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.views import serve
 from django.urls import path, include
 
+
 urlpatterns = [
+    path('', serve, kwargs={'path': 'index.html'}),
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api/', include('api.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
